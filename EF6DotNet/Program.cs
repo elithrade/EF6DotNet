27a80506c81e295443670879f3c1fa935ed6ee1a
Bin Hu <elithrade@gmail.com>
2019-07-13 23:44:07 +0800

    Initial commit

---
+++ EF6DotNet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace EF6DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["SchoolContext"].ConnectionString;
+            using (var ctx = new SchoolContext(connectionString))
+            {
+                var bill = new Student() { StudentName = "Bill" };
+        
+                ctx.Students.Add(bill);
+                ctx.SaveChanges();                
+            }
+        }
+    }
+}
---
+++ EF6DotNet/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace EF6DotNet
+{
+    public class SchoolContext: DbContext 
+    {
+        public SchoolContext(string connectionString): base(connectionString)
+        {
+            
+        }
+            
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
