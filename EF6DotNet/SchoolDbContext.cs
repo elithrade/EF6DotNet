2306d6238280be2a8531804ad82b99122ebffa35
Bin Hu <elithrade@gmail.com>
2019-07-26 13:59:58 +0800

    Fluent API comments

--- EF6DotNet/SchoolDbContext.cs
+++ EF6DotNet/SchoolDbContext.cs
@@ -22,24 +22,27 @@ namespace EF6DotNet
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            // HasRequired, HasOptional, or HasMany method to specify the type of relationship this entity participates in.
+            // WithRequired, WithOptional, and WithMany method to specify the inverse relationship.
 
-            // Configure one to one relationship between Student and StudentInfo
+            // Configure one to one relationship between Student and StudentInfo,
+            // this meaning that the Student entity object must include the StudentInfo
+            // entity object and the StudentInfo entity must include the Student entity. 
             modelBuilder.Entity<Student>()
-                .HasRequired(s => s.Info) // EF Core HasOne(s => s.Info)
-                .WithRequiredPrincipal(i => i.Student); // EF Core WithOne(i => i.Student)
+                .HasRequired(student => student.Info) // EF Core HasOne(s => s.Info)
+                .WithRequiredPrincipal(info => info.Student); // EF Core WithOne(i => i.Student)
 
             // Configure one to many relationship between Course and Enrollment
             modelBuilder.Entity<Enrollment>()
-                .HasRequired(e => e.Course) // Enrollment entity has required the Course property
-                .WithMany(c => c.Enrollments) // Configure the other end that Course entity includes many Enrollment entities
-                .HasForeignKey(e => e.CourseID); // Specify the foreign key
+                .HasRequired(enrollment => enrollment.Course) // Enrollment entity has required the Course property
+                .WithMany(course => course.Enrollments) // Configure the other end that Course entity includes many Enrollment entities
+                .HasForeignKey(enrollment => enrollment.CourseID); // Specify the foreign key
 
             // Similarly one to many relationship between Student and Enrollment, but configure Student instead of Enrollment
             modelBuilder.Entity<Student>()
-                .HasMany(s => s.Enrollments)
-                .WithRequired(e => e.Student)
-                .HasForeignKey(e => e.StudentID);
+                .HasMany(student => student.Enrollments)
+                .WithRequired(enrollment => enrollment.Student)
+                .HasForeignKey(enrollment => enrollment.StudentID);
 
             // Many to many relationship between Student and Teacher
             // Configure both the foreign keys in the joining entity as a composite key using Fluent API.
@@ -52,7 +55,6 @@ namespace EF6DotNet
                     cs.MapRightKey("TeacherID");
                     cs.ToTable("StudentTeacher");
                 });
-
         }
     }
 }
